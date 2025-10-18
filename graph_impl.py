6ac23f8f29225fe547d3cfc156225a27ef75eaf8
Deana-Mendoza <157072867+Deana-Mendoza@users.noreply.github.com>
2025-10-17 23:24:46 -0700

    Commented File!! YIPEEEE

--- graph_impl.py
+++ graph_impl.py
@@ -12,22 +12,27 @@ T = TypeVar('T')
 class Graph(IGraph):
 
     def __init__(self):
+        """Initializes the list of vertices"""
         self._vertices = []
 
     def get_vertices(self) -> List[IVertex]:
+        """Returns all the vertices in self._vertices"""
         return list(self._vertices)
         
     def get_edges(self) -> List[IEdge]:
-        """ collects all edges from vertices """
+        """Gets the edges from all of the vertices in self._vertices"""
         edges: List[IEdge] = []
         for v in self._vertices: 
             edges += v.get_edges()
         return edges
 
     def add_vertex(self, vertex: IVertex) -> None:
+        """Adds a vertex to self.vertices"""
         self._vertices.append(vertex)
     
     def remove_vertex(self, vertex_name: str) -> None:
+        """This removes a vertex if the input name exists then it removes the edges 
+        connected to it """
         for vertex in list(self._vertices):
             if vertex.get_name() == vertex_name:
                 self._vertices.remove(vertex)
@@ -39,7 +44,8 @@ class Graph(IGraph):
                     vertex.remove_edge(edge.get_name())
 
     def add_edge(self,edge: IEdge, from_vertex_name: Optional[str] = None) -> None:
-        """ Add edge if it's connected to the same destination """
+        """ Adds an edge between vertices if they are connected to the same 
+        destination """
         if from_vertex_name:
             for vertex in self._vertices:
                 if vertex.get_name() == from_vertex_name:
@@ -47,7 +53,8 @@ class Graph(IGraph):
                     break
 
     def remove_edge(self, edge_name: str) -> None:
-        """ removes any edges with this name from all vertices """
+        """Iterates through the list of vertices and then their edges
+           to see if the edge name exists and then removes the edge"""
         for v in self._vertices:
             for e in list(v.get_edges()):
                 if e.get_name() == edge_name:
@@ -56,6 +63,7 @@ class Graph(IGraph):
 class Vertex(IVertex):
 
     def __init__(self, name: str) -> None:
+        """Initialization for Vertex class"""
         self.name = name
         self._edges: List[IEdge[T]] = []
         self._visited: bool = False
@@ -63,47 +71,59 @@ class Vertex(IVertex):
         self._data: Optional[T] = None
 
     def get_name(self) -> str:
+        """Gets the vertex name"""
         return self.name
 
     def set_name(self, name: str) -> None:
+        """Sets the name of a vertex based on input"""
         self.name = name
 
     def add_edge(self, edge: IEdge[T]) -> None:
+        """Appends an edge to list of edges"""
         self._edges.append(edge)
 
     def remove_edge(self, edge_name: str) -> None:
-        """ removes the edge by name """
+        """Removes an edge if the edge name exists in the list of edges""" 
         for e in list(self._edges):
             if e.get_name() == edge_name:
                 self._edges.remove(e)
 
     def get_edges(self) -> List[IEdge]:
+        """Gets the list of edges"""
         return list(self._edges)
 
     def set_visited(self, visited: bool) -> None:
+        """Sets whether a vertex has been vistited"""
         self._visited = visited 
 
     def is_visited(self) -> bool:
+        """Checks to see if vertex has been visited"""
         return self._visited
 
 class Edge(IEdge):
     
     def __init__(self, name: str, destination: IVertex, weight: float = 0) -> None:
+        """Initialization for Edge class"""
         self._name = name
         self._destination: IVertex = destination
         self._weight = weight
 
     def get_name(self) -> str:
+        "Gets the name of the edge"
         return self._name
     
     def set_name(self, name: str) -> None:
+        """Changes/sets the name of an edge"""
         self._name = name
 
     def get_destination(self) -> IVertex:
+        """Gets the destination """
         return self._destination
     
     def get_weight(self) -> float:
+        """Gets the weight of edges"""
         return self._weight
     
     def set_weight(self, weight: float) -> None:
+        """Can change or set the weight of an edge"""
         self._weight = weight
